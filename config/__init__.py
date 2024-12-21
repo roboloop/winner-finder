56ca560df14a8e71cb51b0d5136ebd86e249d9f7
Robo Loop <gladparrot@gmail.com>
2024-12-21 00:13:02 +0000

    Correctly set up of pytesseract. Implemented binary search for the first spin

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,9 @@
+from .config import (
+    READ_STEP,
+)
 from .logger_config import setup_logger
 
 __all__ = [
     "setup_logger",
+    "READ_STEP",
 ]
--- stream/frame.py
+++ stream/frame.py
@@ -1,17 +1,27 @@
 from __future__ import annotations
 
-from typing import List, Optional
+import logging
+from typing import Optional
 
 import cv2
 import numpy as np
+import pytesseract
+
+import config
+
+logger = config.setup_logger(level=logging.INFO)
+
 
 class Frame:
     _wheel: Optional[np.ndarray]
+    _lot_name: Optional[str]
+    _init_text = ["победитель", "winner"]
 
     def __init__(self, frame: np.ndarray, index: int):
         self._frame = frame
         self._index = index
         self._wheel = None
+        self._lot_name = None
 
     @property
     def frame(self) -> np.ndarray:
@@ -53,3 +63,42 @@ class Frame:
 
         raise Exception("cannot detect wheel")
 
+    def detect_lot_name(self) -> str:
+        if self._lot_name is not None:
+            return self._lot_name
+
+        center_x, center_y, radius = self.wheel
+        roi_y_start = max(0, center_y - radius - 70)
+        roi_y_end = max(0, center_y - radius - 5)
+        roi_x_start = max(0, center_x - radius)
+        roi_x_end = min(self._frame.shape[1], center_x + radius)
+
+        text_roi = self._frame[roi_y_start:roi_y_end, roi_x_start:roi_x_end]
+        gray = cv2.cvtColor(text_roi, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, 170, 255, cv2.THRESH_BINARY)
+        self._lot_name = pytesseract.image_to_string(thresh, lang=config.TESSERACT_LANG, config="--psm 6")
+        logger.info("Lot name was detected", extra={"text": text.strip(), "frame": {self._index}})
+
+        return self._lot_name
+
+    def is_init_frame(self) -> bool:
+        try:
+            text = self.detect_lot_name()
+        except pytesseract.TesseractNotFoundError:
+            logger.error("Tesseract was not found")
+            raise
+        except Exception:
+            return False
+
+        return any(substring in text.lower() for substring in self._init_text)
+
+    def is_spin_frame(self) -> bool:
+        try:
+            text = self.detect_lot_name()
+        except pytesseract.TesseractNotFoundError:
+            logger.error("Tesseract was not found")
+            raise
+        except Exception as e:
+            return False
+
+        return not any(substring in text.lower() for substring in self._init_text)
