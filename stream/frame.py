b588ef929f1be794e2c63e33f6aa70e996f83556
roboloop <12248453+roboloop@users.noreply.github.com>
2024-12-21 02:26:23 +0000

    Found an edge case with a non-elliptical circle, for now just send on error log

--- stream/frame.py
+++ stream/frame.py
@@ -12,6 +12,8 @@ import config
 logger = config.setup_logger(level=logging.INFO)
 
 
+TOP_OFFSET = 70
+
 class Frame:
     _wheel: Optional[np.ndarray]
     _lot_name: Optional[str]
@@ -68,7 +70,7 @@ class Frame:
             return self._lot_name
 
         center_x, center_y, radius = self.wheel
-        roi_y_start = max(0, center_y - radius - 70)
+        roi_y_start = max(0, center_y - radius - TOP_OFFSET)
         roi_y_end = max(0, center_y - radius - 5)
         roi_x_start = max(0, center_x - radius)
         roi_x_end = min(self._frame.shape[1], center_x + radius)
@@ -76,8 +78,11 @@ class Frame:
         text_roi = self._frame[roi_y_start:roi_y_end, roi_x_start:roi_x_end]
         gray = cv2.cvtColor(text_roi, cv2.COLOR_BGR2GRAY)
         _, thresh = cv2.threshold(gray, 170, 255, cv2.THRESH_BINARY)
-        self._lot_name = pytesseract.image_to_string(thresh, lang=config.TESSERACT_LANG, config="--psm 6")
+
+        text = pytesseract.image_to_string(thresh, lang=config.TESSERACT_LANG, config="--psm 6")
         logger.info("Lot name was detected", extra={"text": text.strip(), "frame": {self._index}})
+
+        self._lot_name = text.strip()
 
         return self._lot_name
 
@@ -90,6 +95,7 @@ class Frame:
         except Exception:
             return False
 
+        # search substring 'победитель' или 'winner' in the frame. This prevents some false parsing
         return any(substring in text.lower() for substring in self._init_text)
 
     def is_spin_frame(self) -> bool:
@@ -102,3 +108,31 @@ class Frame:
             return False
 
         return not any(substring in text.lower() for substring in self._init_text)
+
+    # TODO: doesn't work right. It finds a strange contours
+    def is_circle(self):
+        circle = self.extract_circle_content()
+        gray = cv2.cvtColor(circle, cv2.COLOR_BGR2GRAY)
+        _, binary = cv2.threshold(gray, 215, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if not contours:
+            return False
+
+        contour = max(contours, key=cv2.contourArea)
+        if len(contour) < 5:
+            return False
+        ellipse = cv2.fitEllipse(contour)
+        (x, y), (major_axis, minor_axis), angle = ellipse
+        return abs(major_axis - minor_axis) / max(major_axis, minor_axis) < 0.5
+
+    def extract_circle_content(self) -> np.ndarray:
+        center_x, center_y, radius = self.wheel
+
+        mask = np.zeros(self._frame.shape[:2], dtype=np.uint8)
+        cv2.circle(mask, (center_x, center_y), radius, 255, -1)
+
+        circle_content = cv2.bitwise_and(self._frame, self._frame, mask=mask)
+        x1, y1, x2, y2 = center_x - radius, center_y - radius, center_x + radius, center_y + radius
+        cropped = circle_content[max(0, y1) : y2, max(0, x1) : x2]
+
+        return cropped
