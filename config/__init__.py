d3a7ef518b20a4525e35d3fa79fb373d749114ae
Robo Loop <gladparrot@gmail.com>
2024-12-22 00:36:37 +0000

    Debugging enhancements: skipping the first few seconds of the video and visoalizatiovisualization managmentent from CLI

--- config/__init__.py
+++ config/__init__.py
@@ -1,6 +1,7 @@
 from .config import (
     ASK_LENGTH,
     READ_STEP,
+    VISUALIZATION_ENABLED,
 )
 from .logger_config import setup_logger
 
@@ -8,4 +9,5 @@ __all__ = [
     "setup_logger",
     "READ_STEP",
     "ASK_LENGTH",
+    "VISUALIZATION_ENABLED",
 ]
--- main.py
+++ main.py
@@ -7,9 +7,18 @@ import stream
 logger = config.setup_logger(level=logging.INFO)
 
 
+def handle_winner(filepath: Optional[str], fps: int, skip_sec: int, visualize: bool) -> None:
     try:
         logger.info(f"Start handle: {filepath}")
+
+        if visualize:
+            config.VISUALIZATION_ENABLED = True
+            logger.info(f"Visualization is enabled")
+
+        reader = stream.Reader(filepath, fps, skip_sec)
         segment = stream.Segment(reader)
+
+        # The segment is found and ready. Let's find a winner
         segment.detect_winner()
     except Exception as e:
         logger.error("fail", extra={"e": e, "trace": traceback.format_exc()})
@@ -17,12 +26,22 @@ logger = config.setup_logger(level=logging.INFO)
 
 def main():
     parser = argparse.ArgumentParser(description="Detect largest circle in video or image input.")
-    parser.add_argument("filepath", nargs="?", help="Path to the video file")
-    parser.add_argument("--fps", type=int, default=60, help="Frame per seconds")
+    subparsers = parser.add_subparsers(dest="command", help="Sub-commands help")
+
+    winner_parser = subparsers.add_parser("winner", help="Find winner")
+    winner_parser.add_argument("filepath", nargs="?", help="Path to the video file")
+    winner_parser.add_argument("--fps", type=int, default=60, help="Frame per seconds")
+    winner_parser.add_argument("--skip-sec", type=int, default=0, help="Skip seconds")
+    winner_parser.add_argument("--visualize", action="store_true", help="Visualize all steps (for debugging purposes)")
 
     args = parser.parse_args()
 
-    handle_winner(args.filepath or "pipe:0", args.fps)
+    if args.command == "winner":
+        handle_winner(args.filepath or "pipe:0", args.fps, args.skip_sec, args.visualize)
+        return
+
+    parser.print_help()
+
 
 if __name__ == "__main__":
     main()
