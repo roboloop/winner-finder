45301b8b2274c1bdc6a14a24432120468c1b3d70
roboloop <12248453+roboloop@users.noreply.github.com>
2024-12-24 21:34:26 +0000

    Printing the lag time between the current stream and the handling frame

--- config/__init__.py
+++ config/__init__.py
@@ -7,10 +7,12 @@ from .config import (
     TESSERACT_LANG,
     VISUALIZATION_ENABLED,
 )
-from .logger_config import setup_logger
+from .logger_config import add_global_event_time, setup_logger, update_global_event_time
 
 __all__ = [
     "setup_logger",
+    "update_global_event_time",
+    "add_global_event_time",
     "READ_STEP",
     "SPIN_BUFFER_SIZE",
     "MIN_SKIP_OF_WHEEL_SPIN",
