7038e58608fcf8fb4133e70aa6847dc95a6f3f58
roboloop <12248453+roboloop@users.noreply.github.com>
2025-01-03 03:52:10 +0000

    Sectors colorization, much better

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,6 @@
 from .bezier import calculate_x, calculate_y
 from .bezier_gsap import calculate_x_gsap, calculate_y_gsap
+from .colors import generate_diverse_colors
 from .spin import range, range_with_angle
 from .visualizer import draw_circle, draw_contours, draw_lines, visualize
 
@@ -10,6 +11,7 @@ __all__ = [
     "draw_circle",
     "range",
     "range_with_angle",
+    "generate_diverse_colors",
     "draw_contours",
     "draw_lines",
     "calculate_y_gsap",
