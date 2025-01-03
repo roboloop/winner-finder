bca5c347e8b54ab167d3e87d22637d09d1d007a2
roboloop <12248453+roboloop@users.noreply.github.com>
2025-01-03 00:48:25 +0000

    Visualization of the steps in sector parsing

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,7 @@
 from .bezier import calculate_x, calculate_y
 from .bezier_gsap import calculate_x_gsap, calculate_y_gsap
 from .spin import range, range_with_angle
-from .visualizer import draw_circle, visualize
+from .visualizer import draw_circle, draw_contours, draw_lines, visualize
 
 __all__ = [
     "calculate_y",
@@ -10,6 +10,8 @@ __all__ = [
     "draw_circle",
     "range",
     "range_with_angle",
+    "draw_contours",
+    "draw_lines",
     "calculate_y_gsap",
     "calculate_x_gsap",
 ]
--- utils/visualizer.py
+++ utils/visualizer.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 
+import config
+
 
 def draw_circle(frame: np.ndarray, circle: np.ndarray, title: str = "Circle") -> None:
     image = frame.copy()
@@ -11,7 +13,32 @@ def draw_circle(frame: np.ndarray, circle: np.ndarray, title: str = "Circle") ->
     visualize(frame, title)
 
 
+def draw_contours(frame: np.ndarray, contours: np.ndarray, fill: bool = False, title: str = "Contours") -> None:
+    image = frame.copy()
+    thickness = cv2.FILLED if fill else -1
+    for i, contour in enumerate(contours):
+        cv2.drawContours(image, contours, i, (255, 255, 255), thickness)
+
+    visualize(image, title)
+
+
+def draw_lines(frame: np.ndarray, lines: np.ndarray, title: str = "Lines") -> None:
+    image = frame.copy()
+    for i, line in enumerate(lines):
+        if len(line) == 1:
+            x1, y1, x2, y2 = line[0]
+        elif len(line) == 4:
+            x1, y1, x2, y2 = line
+        else:
+            raise Exception("invalid format")
+
+        cv2.line(image, (x1, y1), (x2, y2), (255, 255, 255), 1)
+
+    visualize(image, title)
+
+
 def visualize(frame: np.ndarray, title: str = "Image"):
-    cv2.imshow(title, frame)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    if config.VISUALIZATION_ENABLED:
+        cv2.imshow(title, frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
